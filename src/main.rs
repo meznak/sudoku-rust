c071d1253584520da70d80e67013cc234b206f99
Nathan Plamondon <nplamondon@gmail.com>
2023-07-06 19:39:16 -0700

    Solve an easy sudoku

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::fmt::{Debug, Formatter, self};
+use std::fmt::{Debug, self};
 
 #[derive(Debug, Default)]
     struct Board {
     cells: [[Cell; 9]; 9],
-    solved: bool,
+    solved: usize,
 }
 
 impl fmt::Display for Board {
@@ -32,33 +32,40 @@ impl Board {
     fn initialize(&mut self, input: &str) {
         let mut position = 0;
 
-        for value in input.split(',') {
+    for value in input.split(',') {
+            let row = position / 9;
+            let col = position % 9;
+            self.cells[row][col].row = row;
+            self.cells[row][col].col = col;
             if value != "-" {
-                let row = position / 9;
-                let col = position % 9;
                 self.cells[row][col].options = vec![value.parse()
                     .expect("Initialization string must be comma-separated 1-9 or -")];
                 self.cells[row][col].solved = true;
+                self.solved += 1;
             }
             position += 1;
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Cell {
+    row: usize,
+    col: usize,
     options: Vec<i8>,
     solved: bool,
 }
 
 impl Default for Cell {
-fn default() -> Cell {
-        Cell {
-            options: (1..10)
-                .collect(),
-            solved: false,
+    fn default() -> Cell {
+            Cell {
+                row: 0,
+                col: 0,
+                options: (1..10)
+                    .collect(),
+                solved: false,
+            }
         }
-    }
 }
 
 impl fmt::Display for Cell {
@@ -71,32 +78,81 @@ impl fmt::Display for Cell {
     }
 }
 
+impl Board {
+    fn eliminate_conflicts(&mut self) {
+        let cells = self.cells.clone();
+
+        self.cells.iter_mut()
+            .flat_map(|c| c.iter_mut())
+            .for_each(|c| {
+                if !c.solved {
+                    if c.eliminate_conflicts(&cells) {
+                        self.solved += 1
+                    }
+                }
+            });
+    }
+}
+
 impl Cell {
-    fn get_value(&self) -> Option<i8> {
-        if self.solved {
-            Some(self.options[0])
-        } else {
-            None
+    fn eliminate_conflicts(&mut self, cells: &[[Cell; 9]; 9]) -> bool {
+        // row
+        for cell in &cells[self.row] {
+            if self.check_remove_option(&cell) {
+                return true;
+            }
         }
+
+        // col
+        for cell in &cells[0..9][self.col] {
+            if self.check_remove_option(&cell) {
+                return true;
+            }
+        }
+
+        // square
+        let first_row = self.row / 3 * 3;
+        let last_row = first_row + 2;
+        let first_col = self.col / 3 * 3;
+        let last_col = first_col + 2;
+
+        for row in &cells[first_row..last_row] {
+            for cell in &row[first_col..last_col] {
+                if self.check_remove_option(&cell) {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
+    fn check_remove_option(&mut self, other: &Cell) -> bool {
+            if !(other.row == self.row && other.col == self.col) {
+                if other.solved {
+                    self.options.retain(|&x| x != other.options[0]);
+                }
+
+                if self.options.len() == 1 {
+                    self.solved = true;
+                }
+            }
+
+            self.solved
     }
 }
 
 fn main() {
-    let sample: &str = "5,3,-,-,7,-,-,-,-,6,-,-,1,9,5,-,-,-,-,9,8,-,-,-,6,-,8,-,-,-,6,-,-,-,3,4,-,-,8,-,3,-,-,1,7,-,-,-,2,-,-,-,6,-,6,-,-,-,2,8,-,-,-,4,1,9,-,-,5,-,-,-,8,-,-,7,9";
-    let mut c = Board::new();
-    c.initialize(&sample);
+    // let sample = "1,2,3,4,5,6,7,8,-,2,-,-,-,-,-,-,-,-,3,-,-,-,-,-,-,-,-,4,-,-,-,-,-,-,-,-,5,-,-,-,-,-,-,-,-,6,-,-,-,-,-,-,-,-,7,-,-,-,-,-,-,-,-,8,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-,-";
+    let sample = "6,-,5,-,-,-,-,2,-,-,-,7,9,6,4,-,-,5,-,9,1,-,2,-,-,-,-,-,8,-,5,9,6,-,-,3,-,3,-,8,-,1,-,5,-,4,-,-,3,7,2,-,6,-,-,-,-,-,8,-,5,9,-,5,-,-,6,1,9,8,-,-,-,1,-,-,-,-,7,-,6";
+    // let sample: &str = "5,3,-,-,7,-,-,-,-,6,-,-,1,9,5,-,-,-,-,9,8,-,-,-,6,-,8,-,-,-,6,-,-,-,3,4,-,-,8,-,3,-,-,1,7,-,-,-,2,-,-,-,6,-,6,-,-,-,2,8,-,-,-,4,1,9,-,-,5,-,-,-,8,-,-,7,9";
+    let mut board = Board::new();
+    board.initialize(&sample);
 
-        // [
-        //     [5,3,-1,-1,7,-1,-1,-1,-1],
-        //     [6,-1,-1,1,9,5,-1,-1,-1],
-        //     [-1,9,8,-1,-1,-1,-1,6,-1],
-        //     [8,-1,-1,-1,6,-1,-1,-1,3],
-        //     [4,-1-1,8,-1,3,-1,-1,1],
-        //     [7,-1,-1,-1,2,-1,-1,-1,6],
-        //     [-1,6,-1-1,-1,2,8,-1],
-        //     [-1,-1,4,1,9,-1,-1,5],
-        //     [-1,-1,-1,8,-1,-1,7,9]
-        // ]
+    print!("loaded: {}\n{}", board.solved, board);
 
-    print!("loaded:\n{}", c);
+    while board.solved < 81 {
+        board.eliminate_conflicts();
+        print!("\nSolved: {}\n{}\n", board.solved, board);
+    }
 }
